356122d6d67fe2e53c5d0c0d8996f88d75138fff
grazianognoll <grazianognoll@gmail.com>
2023-08-17 10:00:36 +0100

    Refactoring

---
+++ pam-monitoring/client.py
@@ -0,0 +1,73 @@
+from grpc.aio import UnaryUnaryClientInterceptor, UnaryStreamClientInterceptor, StreamUnaryClientInterceptor, StreamStreamClientInterceptor
+from grpc.aio import AioRpcError, UnaryUnaryCall, UnaryStreamCall, StreamUnaryCall, StreamStreamCall
+
+from prometheus_client import Counter, Histogram, Gauge, Summary
+from interceptor import utils
+class ClientMetricsInterceptor(UnaryUnaryClientInterceptor, UnaryStreamClientInterceptor, StreamUnaryClientInterceptor, StreamStreamClientInterceptor):
+    def __init__(self):
+        self.request_counter = Counter(
+            'grpc_client_requests_total', 'Total client requests', ['grpc_service', 'grpc_method']
+        )
+        self.request_latency = Histogram(
+            'grpc_client_request_latency_seconds', 'Request latency', ['grpc_service', 'grpc_method']
+        )
+        self.error_counter = Counter(
+            'grpc_client_errors_total', 'Total server errors', ['grpc_service', 'grpc_method', 'error_type']
+        )
+        self.request_size_summary = Summary(
+            'grpc_client_request_size_bytes', 'Request size in bytes', ['grpc_service', 'grpc_method']
+        )
+        self.response_size_summary = Summary(
+            'grpc_client_response_size_bytes', 'Response size in bytes', ['grpc_service', 'grpc_method']
+        )
+        self.in_flight_requests_gauge = Gauge(
+            'grpc_client_in_flight_requests', 'In-flight requests', ['grpc_service', 'grpc_method']
+        )
+
+
+    async def intercept_unary_unary(self, continuation, client_call_details, request):
+        return await self._intercept(continuation, client_call_details, request)
+
+    async def intercept_unary_stream(self, continuation, client_call_details, request):
+        return await self._intercept(continuation, client_call_details, request)
+
+    async def intercept_stream_unary(self, continuation, client_call_details, request_iterator):
+        return await self._intercept(continuation, client_call_details, request_iterator)
+
+    async def intercept_stream_stream(self, continuation, client_call_details, request_iterator):
+        return await self._intercept(continuation, client_call_details, request_iterator)
+
+    async def _intercept(self, continuation, client_call_details, request_or_iterator):
+        method_parts = client_call_details.method.decode('utf-8').split('/')
+        service_name, method_name = method_parts[-2], method_parts[-1]
+
+        self.request_counter.labels(service_name, method_name).inc()
+        self.in_flight_requests_gauge.labels(service_name, method_name).inc()
+
+        request_size = utils.get_size(request_or_iterator)
+        self.request_size_summary.labels(service_name, method_name).observe(request_size)
+
+        call = await continuation(client_call_details, request_or_iterator)
+        try:
+            with self.request_latency.labels(service_name, method_name).time():
+                if isinstance(call, (UnaryUnaryCall, StreamUnaryCall)):
+                    response = await call
+                    response_size = utils.get_size(response)
+                    self.response_size_summary.labels(service_name, method_name).observe(response_size)
+                    return response
+                elif isinstance(call, (UnaryStreamCall, StreamStreamCall)):
+                    async def response_iterator():
+                        async for resp in call:
+                            response_size = utils.get_size(resp)
+                            self.response_size_summary.labels(service_name, method_name).observe(response_size)
+                            yield resp
+                    return response_iterator()
+        except AioRpcError as e:
+            self.error_counter.labels(service_name, method_name, str(e.code())).inc()
+            raise
+        except Exception as e:
+            self.error_counter.labels(service_name, method_name, type(e).__name__).inc()
+            raise
+        finally:
+            self.in_flight_requests_gauge.labels(service_name, method_name).dec()
+        
---
+++ pam-monitoring/server.py
@@ -0,0 +1,89 @@
+from prometheus_client import Counter, Histogram, Summary, Gauge
+from grpc_interceptor.exceptions import GrpcException
+from grpc_interceptor.server import AsyncServerInterceptor
+from typing import Callable, Any
+
+import grpc
+import asyncio
+from timeit import default_timer
+from interceptor import utils
+
+from grpc.aio import ServerInterceptor
+
+class AsyncExceptionToStatusInterceptor(AsyncServerInterceptor):
+
+#class ServerMetricsInterceptor(ServerInterceptor):
+    def __init__(self):
+        self.request_counter = Counter(
+            'grpc_server_requests_total', 'Total server requests', ['grpc_service', 'grpc_method']
+        )
+        self.response_counter = Counter(
+            'grpc_server_responses_total', 'Total server responses', ['grpc_service', 'grpc_method', 'grpc_status']
+        )
+        self.request_latency = Histogram(
+            'grpc_server_request_latency_seconds', 'Request latency', ['grpc_service', 'grpc_method']
+        )
+        self.request_size = Summary(
+            'grpc_server_request_size_bytes', 'Request size in bytes', ['grpc_service', 'grpc_method']
+            )
+        self.response_size = Summary(
+            'grpc_server_response_size_bytes', 'Response size in bytes', ['grpc_service', 'grpc_method']
+        )
+        self.active_requests = Gauge(
+            'grpc_server_active_requests', 'Number of active requests', ['grpc_service', 'grpc_method']
+        )
+
+    async def intercept(
+        self,
+        method: Callable,
+        request_or_iterator: Any,
+        context: grpc.ServicerContext,
+        method_name: str,
+    ) -> Any:
+        try:
+            start = default_timer()
+            service_name, service_method = method_name.split('/')[-2:]
+            self.request_counter.labels(service_name, service_method).inc()
+
+            request_size = utils.get_size(request_or_iterator)
+
+            self.request_size.labels(service_name, service_method).observe(request_size)
+            self.active_requests.labels(service_name, service_method).inc()
+
+            response_or_iterator = method(request_or_iterator, context)
+            if not hasattr(response_or_iterator, "__aiter__"):
+                # Unary, just await and return the response
+                return await response_or_iterator
+        except GrpcException as e:
+            await context.set_code(e.status_code)
+            await context.set_details(e.details)
+
+            raise
+        finally:
+            self.request_latency.labels(service_name, service_method).observe(max(default_timer() - start, 0))
+            response_size = utils.get_size(response_or_iterator) if utils.GetStatusFromCode(context.code()) == grpc.StatusCode.OK else 0
+            self.response_size.labels(service_name, service_method).observe(response_size)
+            self.response_counter.labels(service_name, service_method, str(utils.GetStatusFromCode(context.code()))).inc()
+            self.active_requests.labels(service_name, service_method).dec()
+        # Server streaming responses, delegate to an async generator helper.
+        # Note that we do NOT await this.
+        return self._intercept_streaming(response_or_iterator, context, service_name, service_method, start)
+
+
+    async def _intercept_streaming(self, iterator, context, service_name, service_method, start):
+
+        response_size = 0
+        try:
+            async for r in iterator:
+                yield r
+                response_size += utils.get_size(r)
+
+        except GrpcException as e:
+            await context.set_code(e.status_code)
+            await context.set_details(e.details)
+            raise
+        finally:
+            self.request_latency.labels(service_name, service_method).observe(max(default_timer() - start, 0))
+            self.response_size.labels(service_name, service_method).observe(response_size)
+            self.response_counter.labels(service_name, service_method, utils.GetStatusFromCode(context.code())).inc()
+            self.active_requests.labels(service_name, service_method).dec()
